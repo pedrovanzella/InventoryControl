bd10dd9a89704e1d70c2cee05e43d6562c395a22
Pedro Vanzella <ppvanzella@gmail.com>
2010-08-20 21:43:02 -0300

    trying again. Failing miserably.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InventoryControl::Application.config.secret_token = '8703600fdd9258bd4fbd5be94469f11cda5397558a744944436992a0b717984553f3046712e2cd2f953e4e4d5946bb73ca0390ac4f86994d1825bba1cc10e994'
